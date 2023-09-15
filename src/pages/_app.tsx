ddc363b128cca1d62c0cc168aef9ea3f310cc27e
Alik_Avizha <aleksandravizha@icloud.com>
2023-09-15 21:40:04 +0300

    feat: add styles, start fsd structure

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,11 @@ import type { ReactElement, ReactNode } from 'react'
 import { NextPage } from 'next'
 import type { AppProps } from 'next/app'
 
-import '@/styles/globals.scss'
+import '@fontsource/inter/400.css'
+import '@fontsource/inter/500.css'
+import '@fontsource/inter/600.css'
+import '@fontsource/inter/700.css'
+import '@/styles/index.scss'
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode
