b803f27172e61c7274d35d8f85c6f6cf27d04ec6
belozerov-egor <belaz.egor@gmail.com>
2023-09-16 23:39:43 +0300

    feat: add auth logic and redux

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,18 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import { Provider } from 'react-redux'
+
+import { store } from '@/redux/store'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
+    <Provider store={store}>
+      <Html lang="en">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    </Provider>
   )
 }
