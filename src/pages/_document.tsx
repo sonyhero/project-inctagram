cf1d8b259cef922ff3b730212e0c546a2e80363e
sonyhero <27anton06@gmail.com>
2023-09-17 12:08:41 +0300

    refactor: add store-provider

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,18 +1,13 @@
-import { Html, Head, Main, NextScript } from 'next/document'
-import { Provider } from 'react-redux'
-
-import { store } from '@/redux/store'
+import { Head, Html, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Provider store={store}>
-      <Html lang="en">
-        <Head />
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    </Provider>
+    <Html lang="en">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
   )
 }
