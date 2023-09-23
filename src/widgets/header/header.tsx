c7bd1bfd4737f4e4f673f022473fce667f028bff
sonyhero <27anton06@gmail.com>
2023-09-23 20:14:22 +0300

    feat: add header

--- src/widgets/header/header.tsx
+++ src/widgets/header/header.tsx
@@ -2,12 +2,16 @@ import React from 'react'
 
 import s from './header.module.scss'
 
+import { Bell } from '@/shared/ui/icons'
 import { Typography } from '@/shared/ui/typography'
 
 export const Header = () => {
   return (
     <header className={s.header}>
-      <Typography variant={'large'}>Inctargram</Typography>
+      <div className={s.container}>
+        <Typography variant={'large'}>Inctargram</Typography>
+        <Bell />
+      </div>
     </header>
   )
 }
