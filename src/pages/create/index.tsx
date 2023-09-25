83f83661de08c91c21811dcc5548efc72ff9dce1
sonyhero <27anton06@gmail.com>
2023-09-25 16:29:52 +0300

    fix: base layout

---
+++ src/pages/create/index.tsx
@@ -0,0 +1,8 @@
+import { getBaseLayout } from '@/providers/layout/layout'
+
+const CreatePAge = () => {
+  return <div>Create</div>
+}
+
+export default CreatePAge
+CreatePAge.getLayout = getBaseLayout
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,8 @@
+import { getBaseLayout } from '@/providers/layout/layout'
+
+const SignIn = () => {
+  return <div>Home</div>
+}
+
+export default SignIn
+SignIn.getLayout = getBaseLayout
