cfe15b037ec6a96b5bf119352f896a2e6319985e
sonyhero <27anton06@gmail.com>
2023-10-23 16:29:54 +0300

    refactor: useCreateNewPassword

--- src/features/auth/ui/create-new-password-form/CreateNewPasswordForm.tsx
+++ src/features/auth/ui/create-new-password-form/CreateNewPasswordForm.tsx
@@ -1,65 +1,20 @@
-import React, { FC } from 'react'
+import React from 'react'
 
 import { DevTool } from '@hookform/devtools'
-import { zodResolver } from '@hookform/resolvers/zod'
-import { useRouter } from 'next/router'
-import { useForm } from 'react-hook-form'
-import { toast } from 'react-toastify'
-import { z } from 'zod'
 
 import s from './CreateNewPasswordForm.module.scss'
 
-import { useNewPasswordMutation } from '@/features/auth'
+import { useCreateNewPassword } from '@/features/auth/ui/create-new-password-form/hooks/useCreateNewPassword'
 import { useTranslation } from '@/shared/hooks/useTranstaion'
 import { Button, Card, ControlledTextField, Typography } from '@/shared/ui'
 
-const createNewPasswordSchema = z
-  .object({
-    newPassword: z
-      .string()
-      .min(6)
-      .max(20)
-      .regex(
-        /^(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z])(?=.*[!"#$%&'()*+,-./:;<=>?@[\]^_{|}~])[A-Za-z0-9!"#$%&'()*+,-./:;<=>?@[\]^_{|}~]+$/,
-        'Password must contain a-z, A-Z,  ! " # $ % & \' ( ) * + , - . / : ; < = > ? @ [ \\ ] ^ _` { | } ~'
-      ),
-    passwordConfirm: z.string(),
-  })
-  .refine(data => data.newPassword === data.passwordConfirm, {
-    path: ['passwordConfirm'],
-    message: "Password don't match",
-  })
-
-type CreateNewPasswordFormShem = z.infer<typeof createNewPasswordSchema>
-type PropsType = {
+type Props = {
   recoveryCode: string
 }
-export const CreateNewPasswordForm: FC<PropsType> = ({ recoveryCode }) => {
-  const router = useRouter()
-  const [newPassword] = useNewPasswordMutation()
-  const { control, handleSubmit, setError } = useForm<CreateNewPasswordFormShem>({
-    resolver: zodResolver(createNewPasswordSchema),
-    mode: 'onBlur',
-  })
-  const { t } = useTranslation()
 
-  const onSubmit = (data: CreateNewPasswordFormShem) => {
-    newPassword({ newPassword: data.newPassword, recoveryCode })
-      .unwrap()
-      .then(() => {
-        toast.success('Success')
-        router.push('sign-in')
-      })
-      .catch(err => {
-        err.data.messages.map((el: any) => {
-          setError(`${err.data.messages[0].field}` as 'root', {
-            type: 'server',
-            message: el.message,
-          })
-        })
-      })
-  }
-  const handleSubmitForm = handleSubmit(onSubmit)
+export const CreateNewPasswordForm = ({ recoveryCode }: Props) => {
+  const { handleSubmitForm, control } = useCreateNewPassword(recoveryCode)
+  const { t } = useTranslation()
 
   return (
     <Card className={s.createNewPasswordBlock}>
@@ -87,7 +42,7 @@ export const CreateNewPasswordForm: FC<PropsType> = ({ recoveryCode }) => {
         <Typography variant={'regular14'} className={s.formText}>
           {t.auth.createNewPassword.description}
         </Typography>
-        <Button fullWidth={true} className={s.submit} type="submit">
+        <Button fullWidth={true} className={s.submit} type={'submit'}>
           {t.auth.createNewPassword.createNewPassword}
         </Button>
       </form>
