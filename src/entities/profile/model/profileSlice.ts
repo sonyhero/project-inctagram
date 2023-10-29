bbdd427c7d274521683800539f156372d4842fa8
Alik_Avizha <aleksandravizha@icloud.com>
2023-10-29 16:34:28 +0300

    feat: work with add post modal (add photo, delete photo)

--- src/entities/profile/model/profileSlice.ts
+++ src/entities/profile/model/profileSlice.ts
@@ -1,15 +1,23 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 
+type PostType = {
+  id: string
+  photo: FormData
+}
+
 const initialState = {
-  photos: [] as FormData[],
+  photos: [] as PostType[],
 }
 
 export const profileSlice = createSlice({
   name: 'profileSlice',
   initialState,
   reducers: {
-    setPhoto: (state, action: PayloadAction<FormData>) => {
+    setPhoto: (state, action: PayloadAction<PostType>) => {
       state.photos = [...state.photos, action.payload]
+    },
+    deletePhoto: (state, action: PayloadAction<{ id: string }>) => {
+      state.photos = state.photos.filter(el => el.id !== action.payload.id)
     },
   },
 })
