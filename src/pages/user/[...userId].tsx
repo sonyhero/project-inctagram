b8b716c8a776cab01db1122a5ee596870c284cf2
sonyhero <27anton06@gmail.com>
2023-11-20 16:45:46 +0300

    fix: unauthorized profile content

--- src/pages/user/[...userId].tsx
+++ src/pages/user/[...userId].tsx
@@ -1,9 +1,11 @@
-import { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 import Image from 'next/image'
 import { useRouter } from 'next/router'
 
 import imageIcon from '/public/imageIcon.svg'
+
+import s from '../../widgets/profile-info/ui/ProfileInfo.module.scss'
 
 import {
   getPostsRunningQueriesThunk,
@@ -12,17 +14,19 @@ import {
   useGetPublicPostByIdQuery,
   useGetPublicPostsByUserIdQuery,
 } from '@/entities/posts'
+import { useTranslation } from '@/shared/hooks'
 import { getBaseLayout } from '@/shared/providers'
 import { wrapper } from '@/shared/store'
-import { Modal } from '@/shared/ui'
+import { Button, Modal, Typography } from '@/shared/ui'
 
 export const getServerSideProps = wrapper.getServerSideProps(store => async context => {
   const query = context.query
 
-  const { userId } = query
+  const userId = query.userId?.[0]
+  const postId = query.userId?.[1]
 
-  store.dispatch(getPublicPostsByUserId.initiate({ userId: Number(userId?.[0]) }))
-  store.dispatch(getPublicPostById.initiate({ postId: Number(userId?.[1]) }))
+  store.dispatch(getPublicPostsByUserId.initiate({ userId: Number(userId) }))
+  store.dispatch(getPublicPostById.initiate({ postId: Number(postId) }))
   await Promise.all(store.dispatch(getPostsRunningQueriesThunk()))
 
   return {
@@ -32,7 +36,14 @@ export const getServerSideProps = wrapper.getServerSideProps(store => async cont
 
 export default function UserPage() {
   const { query, push } = useRouter()
+  const userId = query.userId?.[0]
+  const postId = query.userId?.[1]
+  const { t } = useTranslation()
   const [openModal, setOpenModal] = useState(false)
+  const { data: userData } = useGetPublicPostsByUserIdQuery({ userId: Number(userId) })
+  const { data: postData } = useGetPublicPostByIdQuery({ postId: Number(postId) })
+
+  console.log(query.userId?.[1])
 
   useEffect(() => {
     setOpenModal(true)
@@ -43,11 +54,7 @@ export default function UserPage() {
     // push(`/`)
   }
 
-  const { data: userData } = useGetPublicPostsByUserIdQuery({ userId: Number(query.userId?.[0]) })
-
-  const { data: postData } = useGetPublicPostByIdQuery({ postId: Number(query.userId?.[1]) })
-
-  const mappedPosts = userData?.posts.items.map(post => {
+  const mappedPosts = userData?.posts.items.slice(0, 4).map(post => {
     return (
       <div key={post.id}>
         <Image
@@ -56,6 +63,7 @@ export default function UserPage() {
           alt={'post picture'}
           width={200}
           height={200}
+          className={s.post}
         />
       </div>
     )
@@ -65,41 +73,63 @@ export default function UserPage() {
   console.log('postData:', postData)
 
   return (
-    <div>
-      Hello, it is user!
-      <div>
-        <Image
-          priority={true}
-          src={userData?.profile.avatars[0].url ?? imageIcon}
-          alt={'user avatar'}
-          width={400}
-          height={400}
-          style={{
-            width: '200px',
-            height: '200px',
-            borderRadius: '50%',
-          }}
-        />
+    <div className={s.profileBlock}>
+      <div className={s.mainInfo}>
+        <div className={s.photoBlock}>
+          <Image
+            src={userData?.profile.avatars[0].url ?? imageIcon}
+            priority={true}
+            width={192}
+            height={192}
+            className={s.photo}
+            alt={'profilePhoto'}
+          />
+        </div>
+        <div className={s.descriptionBlock}>
+          <div className={s.nameAndSettings}>
+            <Typography variant={'h1'}>{userData?.profile.userName}</Typography>
+            {/*<Button variant={'secondary'} onClick={showProfileSettingsHandler}>*/}
+            {/*  {t.myProfile.profilePage.profileSettings}*/}
+            {/*</Button>*/}
+          </div>
+          <div className={s.statistic}>
+            <div>
+              <Typography variant={'bold14'}>0</Typography>
+              <Typography variant={'regular14'}>{t.myProfile.profilePage.following}</Typography>
+            </div>
+            <div>
+              <Typography variant={'bold14'}>0</Typography>
+              <Typography variant={'regular14'}>{t.myProfile.profilePage.followers}</Typography>
+            </div>
+            <div>
+              <Typography variant={'bold14'}>{userData?.posts.totalCount}</Typography>
+              <Typography variant={'regular14'}>{t.myProfile.profilePage.publications}</Typography>
+            </div>
+          </div>
+          <div className={s.aboutMe}>
+            <Typography variant={'regular16'}>{userData?.profile.aboutMe}</Typography>
+          </div>
+        </div>
       </div>
-      <div>Publications: {userData?.posts.totalCount}</div>
-      <div>About ME: {userData?.profile.aboutMe}</div>
-      {mappedPosts}
-      <Modal open={openModal} onClose={onCloseHandler}>
-        {postData ? (
-          <>
-            <Image
-              priority={true}
-              src={postData.posts.images[0].url}
-              alt={'post picture'}
-              width={200}
-              height={200}
-            />
-            Description: {postData.posts.description}
-          </>
-        ) : (
-          'Post not found!'
-        )}
-      </Modal>
+      <div className={s.postsBlock}>{mappedPosts}</div>
+      {postId && (
+        <Modal open={openModal} onClose={onCloseHandler}>
+          {postData ? (
+            <>
+              <Image
+                priority={true}
+                src={postData.posts.images[0].url}
+                alt={'post picture'}
+                width={200}
+                height={200}
+              />
+              Description: {postData.posts.description}
+            </>
+          ) : (
+            'Post not found!'
+          )}
+        </Modal>
+      )}
     </div>
   )
 }
