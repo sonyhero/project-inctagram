088557c6f6061af8b2ea51dc04ef738008bb1bcf
belozerov-egor <belaz.egor@gmail.com>
2023-12-02 16:07:31 +0300

    refactor: fix styles, component and svg

--- src/widgets/profile-settings/ui/account-management/AccountManagement.tsx
+++ src/widgets/profile-settings/ui/account-management/AccountManagement.tsx
@@ -1,68 +1,65 @@
 import { useState } from 'react'
 
 import s from './AccountManagement.module.scss'
+import { AccountTypeOptions, SubscriptionOptionsType } from './AccountManagement.types'
+import { CurrentSubscription } from './current-subscription'
 
 import { useCreateSubscriptionMutation } from '@/entities/subscription/subscriptionApi'
-import { CheckBox, Paypal, Stripe, Typography } from '@/shared/ui'
+import { Paypal, Stripe, Typography } from '@/shared/ui'
 import { RadioGroupDemo } from '@/shared/ui/radio-group'
 
-const accountTypeOptions = [
+const accountTypeOptions: AccountTypeOptions[] = [
   { id: 1, value: 'Personal' },
   { id: 2, value: 'Business' },
 ]
 
-const subscriptionOptions = [
-  { id: 1, value: '$10 per 1 Day', amount: 10 },
-  { id: 2, value: '$50 per 7 Day', amount: 50 },
-  { id: 3, value: '$100 per month', amount: 100 },
+const subscriptionOptions: SubscriptionOptionsType[] = [
+  { id: 0, value: '$10 per 1 Day', amount: 10, typeSubscription: 'DAY' },
+  { id: 1, value: '$50 per 7 Day', amount: 50, typeSubscription: 'WEEKLY' },
+  { id: 2, value: '$100 per month', amount: 5, typeSubscription: 'MONTHLY' },
 ]
 
 export const AccountManagement = () => {
   const [createSub] = useCreateSubscriptionMutation()
 
-  const [accountType, setAccountType] = useState(accountTypeOptions[0].id)
-  const [subscriptionAmount, setSubscriptionAmount] = useState(subscriptionOptions[0].id)
+  const [accountType, setAccountType] = useState<number>(accountTypeOptions[0].id)
+  const [subscriptionAmount, setSubscriptionAmount] = useState<number>(subscriptionOptions[0].id)
 
-  const paypalHandler = () => {
+  const paypalHandler = (paymentType: 'STRIPE' | 'PAYPAL') => {
     createSub({
-      typeSubscription: 'MONTHLY',
-      paymentType: 'STRIPE',
-      amount: 100,
-      baseUrl: 'https://localhost:3000/my-profile',
+      typeSubscription: subscriptionOptions[subscriptionAmount].typeSubscription,
+      paymentType: paymentType,
+      amount: subscriptionOptions[subscriptionAmount].amount,
+      baseUrl: 'http://localhost:3000/',
     })
+      .unwrap()
+      .then(res => window.location.assign(res.url))
   }
 
   return (
     <div className={s.accountManagement}>
-      <Typography variant={'h3'}>Current Subscription:</Typography>
-      <div>
-        <div>
-          <Typography color={'secondary'} variant={'regular14'}>
-            Expire at
-          </Typography>
-          <Typography variant={'medium14'}>01.12.2023</Typography>
-        </div>
-        <div>
-          <Typography color={'secondary'} variant={'regular14'}>
-            Next payment
-          </Typography>
-          <Typography variant={'medium14'}>13.02.2024</Typography>
-        </div>
-        <CheckBox label={'Auto-Renewal'} />
+      <CurrentSubscription />
+      <Typography variant={'h3'} className={s.radioHead}>
+        Account type:
+      </Typography>
+      <div className={s.radioGroup}>
+        <RadioGroupDemo options={accountTypeOptions} onChangeOption={setAccountType} />
       </div>
-      <Typography variant={'h3'}>Account type:</Typography>
-      <RadioGroupDemo options={accountTypeOptions} onChangeOption={setAccountType} />
       {accountType === 2 && (
         <div>
-          <Typography variant={'h3'}>Your subscription costs:</Typography>
-          <RadioGroupDemo options={subscriptionOptions} onChangeOption={setSubscriptionAmount} />
+          <Typography variant={'h3'} className={s.radioHead}>
+            Change your subscription:
+          </Typography>
+          <div className={s.radioGroup}>
+            <RadioGroupDemo options={subscriptionOptions} onChangeOption={setSubscriptionAmount} />
+          </div>
         </div>
       )}
       <div className={s.paymentsBlock}>
         <div className={s.payments}>
-          <Paypal width={96} height={64} onClick={paypalHandler} />
+          <Paypal width={96} height={64} onClick={() => paypalHandler('PAYPAL')} />
           <Typography variant={'regular14'}>Or</Typography>
-          <Stripe width={96} height={64} />
+          <Stripe width={96} height={64} onClick={() => paypalHandler('STRIPE')} />
         </div>
       </div>
     </div>
