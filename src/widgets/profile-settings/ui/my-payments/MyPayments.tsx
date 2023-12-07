d66a384360d962209baf2c1a91dd0e520b1b328f
sonyhero <27anton06@gmail.com>
2023-12-07 15:47:16 +0300

    feat: payments pagination

--- src/widgets/profile-settings/ui/my-payments/MyPayments.tsx
+++ src/widgets/profile-settings/ui/my-payments/MyPayments.tsx
@@ -2,14 +2,35 @@ import React from 'react'
 
 import { useRouter } from 'next/router'
 
-import { useMyPaymentsQuery } from '@/entities/subscription/subscriptionApi'
-import { Typography } from '@/shared/ui'
+import s from './MyPayments.module.scss'
+
+import { useMyPaymentsQuery } from '@/entities/subscription/api/subscriptionApi'
+import { setCurrentPage, setPageSize } from '@/entities/subscription/model/subscriptionSlice'
+import { useAppDispatch, useAppSelector } from '@/shared/store'
+import { Pagination, Typography } from '@/shared/ui'
+import { SelectBox } from '@/shared/ui/select/SelectBox'
 import { Body, Cell, Head, HeadCell, Root, Row } from '@/shared/ui/table'
 import { getNumericDayMonthTime } from '@/shared/utils'
 
 export const MyPayments = () => {
   const { data: myPaymentsData } = useMyPaymentsQuery()
   const { locale } = useRouter()
+  const dispatch = useAppDispatch()
+
+  const paginationOptions = useAppSelector(state => state.subscriptionSlice.paginationOptions)
+  const currentPage = useAppSelector(state => state.subscriptionSlice.currentPage)
+  const pageSize = useAppSelector(state => state.subscriptionSlice.pageSize)
+
+  const pagesCount = myPaymentsData ? Math.ceil(myPaymentsData.length / pageSize) : 0
+
+  const getPageData = (pageNumber: number) => {
+    const startIndex = (pageNumber - 1) * pageSize
+    const endIndex = startIndex + pageSize
+
+    return myPaymentsData?.slice(startIndex, endIndex)
+  }
+
+  const pageData = getPageData(currentPage)
 
   const headOptions = [
     'Date of Payment',
@@ -19,13 +40,21 @@ export const MyPayments = () => {
     'Payment Type',
   ]
 
+  const onSetPerPage = (value: number) => {
+    dispatch(setPageSize(value))
+  }
+
+  const onSetCurrentPage = (value: number) => {
+    dispatch(setCurrentPage(value))
+  }
+
   const headTable = headOptions.map((option, index) => (
     <HeadCell key={index}>
       <Typography variant={'bold14'}>{option}</Typography>
     </HeadCell>
   ))
 
-  const paymentsTableData = myPaymentsData?.map(payment => {
+  const paymentsTableData = pageData?.map(payment => {
     const dateOfPaymentCell = getNumericDayMonthTime(payment.dateOfPayment, locale as string)
 
     const endDateOfSubscriptionCell = getNumericDayMonthTime(
@@ -62,6 +91,17 @@ export const MyPayments = () => {
         </Head>
         <Body>{paymentsTableData}</Body>
       </Root>
+      <div className={s.pagination}>
+        <Pagination count={pagesCount} page={currentPage} onChange={onSetCurrentPage} />
+        <Typography variant={'regular14'}>Показать</Typography>
+        <SelectBox
+          defaultValue={paginationOptions[0].value}
+          options={paginationOptions}
+          onValueChange={onSetPerPage}
+          className={s.selectPagination}
+        />
+        <Typography variant={'regular14'}>На странице</Typography>
+      </div>
     </div>
   )
 }
