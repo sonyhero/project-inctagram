f9fc003d775ea1aba059ce9155d56f0e2397586f
belozerov-egor <belaz.egor@gmail.com>
2023-12-09 13:28:19 +0300

    refactor: add logic for checkbox

--- src/widgets/profile-settings/ui/account-management/current-subscription/CurrentSubscription.tsx
+++ src/widgets/profile-settings/ui/account-management/current-subscription/CurrentSubscription.tsx
@@ -4,18 +4,34 @@ import { useRouter } from 'next/router'
 
 import s from './CurrentSubscription.module.scss'
 
-import { useCurrentSubscriptionsQuery } from '@/entities/subscription/api/subscriptionApi'
+import {
+  useCanceledAutoRenewalMutation,
+  useCurrentSubscriptionsQuery,
+} from '@/entities/subscription/api/subscriptionApi'
 import { CheckBox, Typography } from '@/shared/ui'
 import { getNumericDayMonthTime } from '@/shared/utils'
 
 export const CurrentSubscription = () => {
   const { data } = useCurrentSubscriptionsQuery()
+  const [canceledAutoRenewal, { isLoading }] = useCanceledAutoRenewalMutation()
   const { locale } = useRouter()
 
-  const dateExpireAt = data && getNumericDayMonthTime(data.data[0].dateOfPayment, locale as string)
+  const dateExpireAt =
+    data &&
+    data.data.length > 0 &&
+    getNumericDayMonthTime(data?.data[data.data.length - 1].endDateOfSubscription, locale as string)
 
-  const dateNextPayment =
-    data && getNumericDayMonthTime(data?.data[0].endDateOfSubscription, locale as string)
+  const dateNextPay =
+    data &&
+    data.data.length > 0 &&
+    getNumericDayMonthTime(
+      data.data[data.data.length - 1].endDateOfSubscription,
+      locale as string,
+      true
+    )
+  const canceledAutoRenewalHandler = () => {
+    canceledAutoRenewal()
+  }
 
   return data?.data.length ? (
     <div className={s.block}>
@@ -29,14 +45,21 @@ export const CurrentSubscription = () => {
           </Typography>
           <Typography variant={'medium14'}>{dateExpireAt}</Typography>
         </div>
-        <div>
-          <Typography color={'secondary'} variant={'regular14'} className={s.mainHead}>
-            Next payment
-          </Typography>
-          <Typography variant={'medium14'}>{dateNextPayment}</Typography>
-        </div>
+        {data?.hasAutoRenewal && (
+          <div>
+            <Typography color={'secondary'} variant={'regular14'} className={s.mainHead}>
+              Next payment
+            </Typography>
+            <Typography variant={'medium14'}>{dateNextPay}</Typography>
+          </div>
+        )}
       </div>
-      <CheckBox checked={data?.data[0].autoRenewal} label={'Auto-Renewal'} />
+      <CheckBox
+        onChange={canceledAutoRenewalHandler}
+        checked={data?.hasAutoRenewal}
+        label={'Auto-Renewal'}
+        disabled={isLoading || !data?.hasAutoRenewal}
+      />
     </div>
   ) : (
     <></>
