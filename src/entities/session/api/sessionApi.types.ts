59edd726cc0d84cd53f0547abc489842b56915b5
sonyhero <27anton06@gmail.com>
2023-12-11 12:19:30 +0300

    feat: session api

---
+++ src/entities/session/api/sessionApi.types.ts
@@ -0,0 +1,11 @@
+export type SessionsTypeResponse = {
+  deviceId: number
+  ip: string
+  lastActive: Date
+  browserName: string
+  browserVersion: string
+  deviceName: string
+  osName: string
+  osVersion: string
+  deviceType: string
+}
