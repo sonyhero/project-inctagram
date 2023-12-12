21515833654d3c672df9d0c3b84abc127e340f52
sonyhero <27anton06@gmail.com>
2023-12-12 15:52:12 +0300

    feat: add devices icons

---
+++ src/shared/ui/icons/devices/Yandex.tsx
@@ -0,0 +1,18 @@
+import { SVGProps, Ref, forwardRef, memo } from 'react'
+
+const SvgComponent = (props: SVGProps<SVGSVGElement>, ref: Ref<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="36"
+    height="36"
+    viewBox="-8 -3 30 30"
+    fill="#fff"
+    ref={ref}
+    {...props}
+  >
+    <path d="m7.083 14.8-4.098 9.2h-2.986l4.5-9.834c-2.114-1.074-3.525-3.018-3.525-6.614-.005-5.034 3.185-7.551 6.979-7.551h3.858v24h-2.582v-9.2h-2.146zm2.147-12.62h-1.378c-2.08 0-4.097 1.378-4.097 5.372 0 3.858 1.847 5.1 4.097 5.1h1.378z" />
+  </svg>
+)
+const ForwardRef = forwardRef(SvgComponent)
+
+export const YandexIcon = memo(ForwardRef)
---
+++ src/shared/ui/icons/devices/index.ts
@@ -0,0 +1,7 @@
+export { ChromeIcon } from './Chrome'
+export { BrowserIcon } from './Browser'
+export { DesctopIcon } from './Desctop'
+export { FireFoxIcon } from './FireFox'
+export { OperaIcon } from './Opera'
+export { YandexIcon } from './Yandex'
+export { MobileIcon } from './Mobile'
