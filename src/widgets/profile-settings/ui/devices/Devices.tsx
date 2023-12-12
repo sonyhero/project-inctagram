8f9987f76b676bcfdc5a9e16de0357bddc0a9b9d
sonyhero <27anton06@gmail.com>
2023-12-12 17:12:18 +0300

    feat: add logic to devices

--- src/widgets/profile-settings/ui/devices/Devices.tsx
+++ src/widgets/profile-settings/ui/devices/Devices.tsx
@@ -10,6 +10,15 @@ import {
 } from '@/entities/session/api'
 import { useTranslation } from '@/shared/hooks'
 import { Button, LogOut, Typography } from '@/shared/ui'
+import {
+  BrowserIcon,
+  ChromeIcon,
+  DesctopIcon,
+  FireFoxIcon,
+  MobileIcon,
+  OperaIcon,
+  YandexIcon,
+} from '@/shared/ui/icons/devices'
 import { getNumericDayMonthTime } from '@/shared/utils'
 
 export const Devices = () => {
@@ -33,7 +42,7 @@ export const Devices = () => {
       <Typography variant={'h3'} className={s.currentDeviceHead}>
         Current device:
       </Typography>
-      {currentDeviceData && <ActiveDevice {...currentDeviceData} />}
+      {currentDeviceData && <ActiveDevice {...currentDeviceData} isCurrentDevice />}
       <div className={s.buttonBlock}>
         <Button onClick={terminateAllHandler} variant={'outline'}>
           Terminate all other session
@@ -47,13 +56,51 @@ export const Devices = () => {
   )
 }
 
-type Props = Partial<SessionsTypeResponse>
+type Props = Partial<SessionsTypeResponse> & { isCurrentDevice?: boolean }
 
 export const ActiveDevice = (props: Props) => {
-  const { ip, lastActive, deviceType, deviceName, deviceId } = props
+  const {
+    ip,
+    lastActive,
+    deviceType,
+    deviceName,
+    deviceId,
+    browserName,
+    osName,
+    isCurrentDevice = false,
+  } = props
   const [terminateSession] = useTerminateSessionMutation()
   const { locale } = useRouter()
   const { t } = useTranslation()
+
+  const isActiveSession = deviceType && deviceType
+
+  const deviceTypeImage = deviceType === 'mobile' ? <MobileIcon /> : <DesctopIcon />
+
+  const getBrowserImage = (value: string) => {
+    switch (value.toLowerCase()) {
+      case 'chrome':
+        return <ChromeIcon />
+      case 'yandex':
+        return <YandexIcon />
+      case 'opera':
+        return <OperaIcon />
+      case 'firefox':
+        return <FireFoxIcon />
+      default:
+        return <BrowserIcon />
+    }
+  }
+
+  const deviceImage = isCurrentDevice ? getBrowserImage(browserName ?? '') : deviceTypeImage
+
+  const deviceDescription = isCurrentDevice ? (
+    <Typography variant={'bold16'}>{browserName}</Typography>
+  ) : (
+    <Typography variant={'bold16'}>
+      {deviceName} {osName}
+    </Typography>
+  )
 
   const lastVisitDate = lastActive && getNumericDayMonthTime(lastActive, locale as string)
 
@@ -61,22 +108,30 @@ export const ActiveDevice = (props: Props) => {
     deviceId && terminateSession(deviceId)
   }
 
-  const isActiveSession = deviceType && deviceType
-
   return (
-    <div className={s.device}>
-      {deviceType && <div>deviceType:{deviceType}</div>}
-      {deviceName && <div>deviceName:{deviceName}</div>}
-      <div>ip:{ip}</div>
-      <div>last visit:{lastVisitDate}</div>
-      {isActiveSession && (
-        <Button className={s.logout} variant={'text'} onClick={logoutHandler}>
-          <LogOut />
-          <Typography color={'primary'} variant={'medium14'}>
-            {t.sidebar.logout}
-          </Typography>
-        </Button>
-      )}
+    <div className={s.activeDevice}>
+      <div className={s.deviceBlock}>
+        <div>{deviceImage}</div>
+        <div className={s.descriptionBlock}>
+          {deviceDescription}
+          <Typography variant={'bold16'}></Typography>
+          <Typography variant={'regular14'}>IP:{ip}</Typography>
+          {!isCurrentDevice && (
+            <Typography variant={'small'}>Last visit:{lastVisitDate}</Typography>
+          )}
+        </div>
+      </div>
+
+      <div className={s.logoutBlock}>
+        {isActiveSession && (
+          <Button variant={'text'} onClick={logoutHandler}>
+            <LogOut />
+            <Typography color={'primary'} variant={'medium14'}>
+              {t.sidebar.logout}
+            </Typography>
+          </Button>
+        )}
+      </div>
     </div>
   )
 }
