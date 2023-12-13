801ff14c5e41f5a41a5fd6fdeae5048d5ea777f6
sonyhero <27anton06@gmail.com>
2023-12-13 19:09:14 +0300

    fix: protect settings

--- src/pages/profile/settings/devices/index.tsx
+++ src/pages/profile/settings/devices/index.tsx
@@ -1,7 +1,24 @@
+import { useRouter } from 'next/router'
+
+import { useMeQuery } from '@/features/auth'
+import { PATH } from '@/shared/config/routes'
 import { getSettingsLayout } from '@/shared/providers/settings-layout'
 import { Devices } from '@/widgets/profile-settings/ui/devices/Devices'
 
 const DevicesPage = () => {
+  const { data, isLoading } = useMeQuery()
+  const { push } = useRouter()
+
+  if (isLoading) {
+    return <div>...Loading</div>
+  }
+
+  if (!data) {
+    push(PATH.SIGN_IN)
+
+    return
+  }
+
   return <Devices />
 }
 
--- src/pages/profile/settings/payments/index.tsx
+++ src/pages/profile/settings/payments/index.tsx
@@ -1,7 +1,24 @@
+import { useRouter } from 'next/router'
+
+import { useMeQuery } from '@/features/auth'
+import { PATH } from '@/shared/config/routes'
 import { getSettingsLayout } from '@/shared/providers/settings-layout'
 import { MyPayments } from '@/widgets/profile-settings/ui/my-payments/MyPayments'
 
 const PaymentsPage = () => {
+  const { data, isLoading } = useMeQuery()
+  const { push } = useRouter()
+
+  if (isLoading) {
+    return <div>...Loading</div>
+  }
+
+  if (!data) {
+    push(PATH.SIGN_IN)
+
+    return
+  }
+
   return <MyPayments />
 }
 
