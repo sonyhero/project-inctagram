ce0cd8b02bd6b935b04c71dda4c4df961ad14864
sonyhero <27anton06@gmail.com>
2023-12-29 17:48:58 +0300

    feat: add description data base

--- src/shared/config/draftDataBase.ts
+++ src/shared/config/draftDataBase.ts
@@ -4,7 +4,9 @@ import { PostType } from '@/entities'
 
 export type PostDataBaseType = Omit<PostType, 'imageUrl'> & { image: Blob }
 
-export class DB extends Dexie {
+export type DescriptionDataBaseType = { currentDescription: string }
+
+class PostsDB extends Dexie {
   posts!: Table<PostDataBaseType>
 
   constructor() {
@@ -15,10 +17,29 @@ export class DB extends Dexie {
   }
 }
 
-const db = new DB()
+class DescriptionDB extends Dexie {
+  description!: Table<DescriptionDataBaseType>
 
-export const clearDB = () => db.posts.clear()
-export const putDataToDB = (data: PostDataBaseType) => {
-  db.posts.put(data)
+  constructor() {
+    super('DescriptionDataBase')
+    this.version(1).stores({
+      description: 'currentDescription',
+    })
+  }
 }
-export const getDataFromDB = () => db.posts.toArray()
+
+const postsDb = new PostsDB()
+
+const descriptionDB = new DescriptionDB()
+
+export const clearPostsDB = () => postsDb.posts.clear()
+export const putPostsDataToDB = (data: PostDataBaseType) => {
+  postsDb.posts.put(data)
+}
+export const getPostsDataFromDB = () => postsDb.posts.toArray()
+
+export const clearDescriptionDB = () => descriptionDB.description.clear()
+export const putPostDescriptionDataToDB = (data: { currentDescription: string }) => {
+  descriptionDB.description.put(data)
+}
+export const getPostsDescriptionDataFromDB = () => descriptionDB.description.toArray()
