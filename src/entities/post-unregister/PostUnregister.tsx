01c463778f8d803f29e3c6cd6fc0a13293ddf8ad
sonyhero <27anton06@gmail.com>
2024-02-16 19:36:33 +0300

    fix: post unregistered show more description

--- src/entities/post-unregister/PostUnregister.tsx
+++ src/entities/post-unregister/PostUnregister.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import Image from 'next/image'
 import Link from 'next/link'
@@ -18,15 +18,27 @@ type Props = PostsResponseType
 export const PostUnregister = (props: Props) => {
   const { images, description, createdAt, avatarOwner, ownerId, id, owner } = props
   const { locale } = useRouter()
+  const [showMore, setShowMore] = useState<boolean>(false)
 
   const { filterImages, activeImage, prevImage, nextImage, activeIndex, setActiveIndex } =
     usePostImagePagination({ images })
 
+  const collapseHandler = () => {
+    setShowMore(!showMore)
+  }
+
   return (
     <div className={s.postWrapper}>
-      <div className={s.photoBlock}>
+      <div className={`${s.photoBlock} ${showMore && s.collapsePhotoBlock}`}>
         <Link href={`${PATH.USER}/${ownerId}/${id}`}>
-          <Image src={activeImage} priority={true} width={240} height={240} alt={'post picture'} />{' '}
+          <Image
+            className={`${s.postImage} ${showMore && s.collapsePostImage}`}
+            src={activeImage}
+            priority={true}
+            width={240}
+            height={240}
+            alt={'post picture'}
+          />{' '}
         </Link>
         <PhotoPagination
           changePhotoNext={nextImage}
@@ -47,9 +59,15 @@ export const PostUnregister = (props: Props) => {
       <Typography variant={'small'} color={'secondary'}>
         {getDayMonthTime(createdAt, locale ?? 'en')}
       </Typography>
-      <Typography variant={'regular14'} color={'primary'} className={s.description}>
-        {description}
+      <Typography variant={'regular14'} color={'primary'}>
+        {showMore ? description : `${description.substring(0, 90)}`}
       </Typography>
+      {description.length > 90 && (
+        <Typography variant={'link'} onClick={collapseHandler}>
+          {' '}
+          ...{showMore ? 'Hide' : 'Show more'}
+        </Typography>
+      )}
     </div>
   )
 }
