52ac7edfd6aea23474f01092433ef85241684c2e
sonyhero <27anton06@gmail.com>
2024-03-03 11:24:56 +0300

    fix: SettingsLayout

--- src/shared/providers/settings-layout/SettingsLayout.tsx
+++ src/shared/providers/settings-layout/SettingsLayout.tsx
@@ -5,33 +5,16 @@ import { NextPage } from 'next'
 import s from './SettingsLayout.module.scss'
 
 import { SettingsSwitcher } from '@/features/sattings-switcher/SettingsSwitcher'
-import { StoreProvider } from '@/shared/providers/store-provider/StoreProvider'
-import { HeadMeta } from '@/shared/ui/head-meta'
-import { Header } from '@/widgets/header'
-import { SideBar } from '@/widgets/side-bar'
 
-export const SettingsLayout: NextPage<PropsWithChildren> = ({ children }) => {
+const SettingsLayout: NextPage<PropsWithChildren> = ({ children }) => {
   return (
-    <>
-      <HeadMeta title={'Inctagram'} />
-      <Header />
-      <div className={s.container}>
-        <SideBar />
-        <main className={s.main}>
-          <div className={s.profileBlock}>
-            <SettingsSwitcher />
-            {children}
-          </div>
-        </main>
-      </div>
-    </>
+    <div className={s.profileBlock}>
+      <SettingsSwitcher />
+      {children}
+    </div>
   )
 }
 
 export const getSettingsLayout = (page: ReactElement) => {
-  return (
-    <StoreProvider>
-      <SettingsLayout>{page}</SettingsLayout>
-    </StoreProvider>
-  )
+  return <SettingsLayout>{page}</SettingsLayout>
 }
