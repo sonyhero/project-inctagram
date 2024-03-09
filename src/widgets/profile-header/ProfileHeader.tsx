3ddff36f36470aa9acabb2bf795d46387b63142a
sonyhero <27anton06@gmail.com>
2024-03-09 12:16:56 +0300

    refactor: user profile header

--- src/widgets/profile-header/ProfileHeader.tsx
+++ src/widgets/profile-header/ProfileHeader.tsx
@@ -4,41 +4,62 @@ import { useRouter } from 'next/router'
 
 import s from './ProfileHeader.module.scss'
 
-import { Avatar } from '@/entities'
-import { useGetProfileQuery } from '@/entities/profile'
+import { useGetPublicUserProfileByIdQuery } from '@/entities'
+import { AvatarOwner } from '@/entities/avatar-owner'
+import { useMeQuery } from '@/features/auth'
+import { useFollowingUserMutation, useGetUserQuery } from '@/features/following/api'
 import { PATH } from '@/shared/config/routes'
 import { useTranslation } from '@/shared/hooks'
-import { useAppSelector } from '@/shared/store'
 import { Button, Typography } from '@/shared/ui'
 import { ProfileStatistic } from '@/widgets/profile-statistic'
 
-export const ProfileHeader = () => {
-  const { data: profileData } = useGetProfileQuery()
+type Props = {
+  profileId: number
+}
+
+export const ProfileHeader = ({ profileId }: Props) => {
+  const { data: profileData } = useGetPublicUserProfileByIdQuery({ profileId })
+
+  const { data: userData, isSuccess } = useGetUserQuery({ userName: profileData?.userName })
+  const [followUser, { isLoading: isLoadingFollow }] = useFollowingUserMutation()
+
+  const { data: meData } = useMeQuery()
+  const isMe = meData ? meData.userId === profileId : false
+
   const { t } = useTranslation()
   const { push } = useRouter()
   const showProfileSettingsHandler = () => {
     push(PATH.MY_PROFILE_SETTINGS_GENERAL)
   }
 
-  const publicationCount = useAppSelector(state => state.postsSlice.publicationCount)
+  const followingUserHandler = () => {
+    followUser({ selectedUserId: profileId })
+  }
 
   return (
     <div className={s.profileHeader}>
       <div className={s.photoBlock}>
-        <Avatar className={s.photo} />
+        <AvatarOwner width={192} height={192} avatarOwner={profileData?.avatars[0]?.url} />
       </div>
       <div className={s.profileDescription}>
         <div className={s.nameAndSettings}>
           <Typography variant={'h1'}>{profileData?.userName}</Typography>
-          <Button variant={'secondary'} onClick={showProfileSettingsHandler}>
-            {t.myProfile.profilePage.profileSettings}
-          </Button>
+          {isSuccess && (
+            <Button
+              disabled={isLoadingFollow}
+              variant={userData?.isFollowing ? 'outline' : 'primary'}
+              onClick={followingUserHandler}
+            >
+              {userData?.isFollowing ? 'Unfollow' : 'Follow'}
+            </Button>
+          )}
+          {isMe && (
+            <Button variant={'secondary'} onClick={showProfileSettingsHandler}>
+              {t.myProfile.profilePage.profileSettings}
+            </Button>
+          )}
         </div>
-        <ProfileStatistic
-          userName={profileData?.userName}
-          postsCount={publicationCount}
-          aboutMe={profileData?.aboutMe}
-        />
+        <ProfileStatistic userName={profileData?.userName} aboutMe={profileData?.aboutMe} />
       </div>
     </div>
   )
--- src/widgets/profile-statistic/ProfileStatistic.tsx
+++ src/widgets/profile-statistic/ProfileStatistic.tsx
@@ -8,12 +8,11 @@ import { Typography } from '@/shared/ui'
 
 type Props = {
   userName?: string
-  postsCount?: number
   aboutMe?: string
 }
 
 export const ProfileStatistic = (props: Props) => {
-  const { postsCount, aboutMe, userName } = props
+  const { aboutMe, userName } = props
   const { data: userData } = useGetUserQuery({ userName }, { skip: !userName })
   const [getFollowers] = useLazyGetUserFollowersQuery()
   const { t } = useTranslation()
@@ -24,20 +23,22 @@ export const ProfileStatistic = (props: Props) => {
 
   return (
     <>
-      <div className={s.statistic}>
-        <div>
-          <Typography variant={'bold14'}>{userData?.followingCount}</Typography>
-          <Typography variant={'regular14'}>{t.myProfile.profilePage.following}</Typography>
+      {!!userData && (
+        <div className={s.statistic}>
+          <div>
+            <Typography variant={'bold14'}>{userData?.followingCount}</Typography>
+            <Typography variant={'regular14'}>{t.myProfile.profilePage.following}</Typography>
+          </div>
+          <div onClick={getFollowersHandler}>
+            <Typography variant={'bold14'}>{userData?.followersCount}</Typography>
+            <Typography variant={'regular14'}>{t.myProfile.profilePage.followers}</Typography>
+          </div>
+          <div>
+            <Typography variant={'bold14'}>{userData?.publicationsCount}</Typography>
+            <Typography variant={'regular14'}>{t.myProfile.profilePage.publications}</Typography>
+          </div>
         </div>
-        <div onClick={getFollowersHandler}>
-          <Typography variant={'bold14'}>{userData?.followersCount}</Typography>
-          <Typography variant={'regular14'}>{t.myProfile.profilePage.followers}</Typography>
-        </div>
-        <div>
-          <Typography variant={'bold14'}>{postsCount}</Typography>
-          <Typography variant={'regular14'}>{t.myProfile.profilePage.publications}</Typography>
-        </div>
-      </div>
+      )}
       <div className={s.aboutMe}>
         <Typography variant={'regular16'}>{aboutMe}</Typography>
       </div>
